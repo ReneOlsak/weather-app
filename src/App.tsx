f9dc349283a4cb7deb2137632d31bb8756cbd577
René Olšák <rene.olsak@email.cz>
2022-12-23 15:15:20 +0100

    First basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import './App.scss'
 
 function App() {
-
   return (
-    <div className="App">
-      Hello
-    </div>
+    <main className="app">
+      <section className="app-box">
+        <h1 className="title">
+          Weather <span className="title-forecast">Forecast</span>
+        </h1>
+        <p className="title-info">
+          Enter a place you want to know the weather of
+        </p>
+        <div>
+          <input type="text" value={''} className="app-input"></input>
+          <button className="search-button">search</button>
+        </div>
+      </section>
+    </main>
   )
 }
 
