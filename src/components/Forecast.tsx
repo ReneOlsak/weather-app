d0a5dc594a20c06b1f3273bc871da5ee471d3ad4
René Olšák <rene.olsak@email.cz>
2022-12-29 13:55:07 +0100

    Added Icons and basic styling of details

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,13 +1,58 @@
 import { forecastType } from '../types'
-
+import Sunrise from './Icons/Sunrise'
+import Sunset from './Icons/Sunset'
 
 type Props = {
   data: forecastType
 }
+
+const Temperature = ({ temp }: { temp: number }): JSX.Element => (
+  <span>{temp}°C</span>
+)
+
 const Forecast = ({ data }: Props): JSX.Element => {
+  const today = data.list[0]
   return (
-    <div>
-      <p>{data.sunrise}</p>
+    <div className="forecast-container">
+      <div>
+        <section className="forecast-top">
+          <h2 className="forecast-location">
+            {data.name}, {data.country}
+          </h2>
+          <h1>
+            <Temperature temp={Math.round(today.main.temp)} />
+          </h1>
+          <p>
+            {today.weather[0].main} {today.weather[0].description}
+          </p>
+          H: <Temperature temp={Math.ceil(today.main.temp_max)} /> L:{' '}
+          <Temperature temp={Math.floor(today.main.temp_min)} />
+        </section>
+
+        <section className="forecast-cloud-images">
+          {data.list.map((item, i) => (
+            <div key={i}>
+              <p>{i === 0 ? "Now" : new Date(item.dt * 1000).getHours()}</p>
+              <img
+                src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                alt={`weather-icon-${item.weather[0].description}`}
+              />
+              <p>
+                <Temperature temp={Math.round(item.main.temp)} />
+              </p>
+            </div>
+          ))}
+        </section>
+
+        <section className="forecast-details">
+          <div className="sunrise">
+            <Sunrise />
+          </div>
+          <div className="sunset">
+            <Sunset />
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -12,29 +12,31 @@ export type forecastType = {
   list: [
     {
       dt: number
-      feels_like: number
-      humidity: number
-      pressure: number
-      temp: number
-      temp_max: number
-      temp_min: number
+      main: {
+        feels_like: number
+        humidity: number
+        pressure: number
+        temp: number
+        temp_max: number
+        temp_min: number
+      }
+      weather: [
+        {
+          main: string
+          icon: string
+          description: string
+        }
+      ]
+      wind: {
+        speed: number
+        gust: number
+        deg: number
+      }
+      clouds: {
+        all: number
+      }
+      pop: number
+      visibility: number
     }
   ]
-  weather: [
-    {
-      main: string
-      icon: string
-      description: string
-    }
-  ]
-  wind: {
-    speed: number
-    gust: number
-    deg: number
-  }
-  clouds: {
-    all: number
-  }
-  pop: number
-  visibility: number
 }
